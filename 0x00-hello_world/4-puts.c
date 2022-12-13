d317288333f6cc472867541ed6f6c63c28829180
ipy06 <ipinoluwaheze@gmail.com>
2022-12-13 01:36:56 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * This is the Hello Puts Program 
+ *
+ * Return : 0 (Successful Program)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
