40cab5316329876593f726e71ccf9e32154c3352
ipy06 <ipinoluwaheze@gmail.com>
2022-12-15 09:28:04 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print out a specified text using printf
+ *
+ * Return: 0 is to be returned if successful.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
