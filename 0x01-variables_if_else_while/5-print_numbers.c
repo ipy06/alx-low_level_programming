aa3be6dca34174a997e62afbeabcd4656058ab2b
ipy06 <ipinoluwaheze@gmail.com>
2022-12-24 06:15:27 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0 - 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+		printf("%d", ch);
+
+	printf("\n");
+	return (0);
+}
